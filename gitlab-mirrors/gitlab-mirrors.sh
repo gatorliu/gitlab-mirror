99aee0a4131ce73c16927c3a4809cad139479a9f
jliu-FTDM <jliu@ftdm.com.tw>
2023-01-06 14:11:11 +0800

    ssss

--- gitlab-mirrors/gitlab-mirrors.sh
+++ gitlab-mirrors/gitlab-mirrors.sh
@@ -3,13 +3,14 @@
 if [ "x${1}" == "x" ]; then
 	echo "usage:"
     echo "	${0} GitProjectPath"
-	exit;
+	exit 1;
 fi
 
+echo "	${0} GitProjectPath"
 GitProjectPath="/srv/gitlab/FT/mirrors/${1}"
 
 cd $GitProjectPath
 
-#git fetch --prune origin
-#git push --mirror target
+git fetch --prune origin
+git push --mirror target
 
